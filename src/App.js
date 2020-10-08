8a70d2ba4395bde61c1367a556471ee0ea99cd0f
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-10-08 08:10:49 +0300

    Done with player mode handling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Home from "./Components/Home";
+import Difficulty from "./Components/Difficulty";
+import Game from "./Components/Game";
 
 function App() {
-  return <div className="App border my-5">Place your code here</div>;
+  const [mode, setMode] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
+
+  const page = () => {
+    if (difficulty) return <Game difficulty={difficulty} mode={mode} />;
+    if (mode) return <Difficulty setDifficulty={setDifficulty} />;
+    return <Home setMode={setMode} />;
+  };
+
+  return (
+    <div className="App border my-5">
+      {page()}
+      <div className="row">
+        <div className="col-3">
+          <button
+            className="btn btn-danger mb-3"
+            onClick={() => {
+              setMode(null);
+              setDifficulty(null);
+            }}
+          >
+            Reset
+          </button>{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
